a736f261e3e8f3e5bb11230a9063e6b91d5d4528
Eric Moyer <compu.moyer@gmail.com>
2020-06-13 15:22:58 -0400

    Add wiring for using state via a reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,82 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Editor from './Editor';
 import nodes from './nodes';
+import * as Network from './nodes_type';
 
-const language = "en-US";
-const modelName = "Demo Model";
+type WorkbenchState = {
+    /// The beliefs being edited
+    beliefs: {
+        nodes: Network.Nodes;
+        language: string;
+        modelName: string;
+    }
+    /// The key of the currently selected node in beliefs.Nodes
+    selection: string | null;
+    /// The URL for storing the current beliefs
+    currentURL: string | null;
+}
+
+const initialState : WorkbenchState = {
+    "beliefs": {
+        "nodes": nodes,
+        "language": "en-US",
+        "modelName": "Demo Model"
+    },
+    "selection": null,
+    "currentURL": null
+};
+
+enum CommandType {
+    NoOp
+}
+interface Command {
+    type: CommandType;
+}
+
+type WorkbenchReducer = (state: WorkbenchState, action: Command) => WorkbenchState;
+type DispatchTable = WorkbenchReducer[];
+
+const dispatchTable = createDispatchTable();
+
+function createDispatchTable(){
+    let dispatchTable: DispatchTable = [];
+    dispatchTable[CommandType.NoOp] = (state, _action) => state;
+    return dispatchTable;
+}
+
+function log(message: string){
+    console.log(message);
+}
+
+const stateTransition: WorkbenchReducer = (state, action) => {
+    if (action.type in dispatchTable) {
+        return dispatchTable[action.type](state, action);
+    }
+    log("Unknown action:" + action.type + " (" + CommandType[action.type] + ") "
+        + "Original action:" + action);
+    return state;
+};
+
+const DispatchContext = React.createContext((_: Command)=>{});
+
+function BeliefWorkbench() {
+    const [workbenchState, workbenchDispatch] = useReducer(stateTransition, initialState);
+    return <DispatchContext.Provider value={workbenchDispatch}>
+        <React.StrictMode>
+            <Editor
+                nodes={workbenchState.beliefs.nodes}
+                language={workbenchState.beliefs.language}
+                modelName={workbenchState.beliefs.modelName}
+                version="v0.01"
+                selection={workbenchState.selection}
+            />,
+        </React.StrictMode>
+    </DispatchContext.Provider>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Editor
-        nodes={nodes}
-        language={language}
-        modelName={modelName}
-        version="v0.01"
-        selection={Object.keys(nodes)[0]}
-    />,
-  </React.StrictMode>,
+  <BeliefWorkbench />,
   document.getElementById("react-container")
 );
