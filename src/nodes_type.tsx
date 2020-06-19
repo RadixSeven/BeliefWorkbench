aa56edb5a1a543bc55b227ae66d18fe12eb9b078
Eric Moyer <compu.moyer@gmail.com>
2020-06-19 00:15:13 -0400

    Switch node types to use runtypes

--- src/nodes_type.tsx
+++ src/nodes_type.tsx
@@ -1,101 +1,167 @@
-export type DistributionType =
-  | "ContinuousUniform"
-  //    "Gaussian" |
-  //    "PiecewiseConstant" | // Sorted list of bin medians, equal probability
-  //    "EmpiricalBins" | // Like piecewise constant except that the first and last bins are scaled exponential distributionProps rather than scaled continuous uniform
-  | "DiscreteUniform";
+import {
+  Union,
+  Literal,
+  Static,
+  Array,
+  String,
+  Record,
+  Dictionary,
+  Number,
+  Boolean,
+} from "runtypes";
 
-export type NodeType =
-  | "DistributionNode"
-  | "FunctionNode"
-  | "ConstantNode"
-  | "ConstraintNode"
-  | "VisualizationNode";
+export const DistributionTypeR = Union(
+  Literal("ContinuousUniform"),
+  //    Literal("Gaussian"),
+  //    Literal("PiecewiseConstant"), // Sorted list of bin medians, equal probability
+  //    Literal("EmpiricalBins"), // Like piecewise constant except that the first and last bins are scaled exponential distributionProps rather than scaled continuous uniform
+  Literal("DiscreteUniform")
+);
+export type DistributionType = Static<typeof DistributionTypeR>;
+
+export const NodeTypeR = Union(
+  Literal("DistributionNode"),
+  Literal("FunctionNode"),
+  Literal("ConstantNode"),
+  Literal("ConstraintNode"),
+  Literal("VisualizationNode")
+);
+export type NodeType = Static<typeof NodeTypeR>;
 
 /// Different deterministic functions to apply. All of these should do
 /// something reasonable when given inputs of the wrong type. But there
 /// should be an optional strict mode that highlights type errors and
 /// forces the user to do something about them.
-export type FunctionType =
-  | "Add" // Add its numeric inputs
-  | "Multiply" // Multiply its numeric inputs
-  | "Divide" // Divide its numeric inputs by the last one
-  | "Round" // Round its numeric inputs to the nearest integer
-  | "Ceil" // Round up
-  | "Floor" // Round down
-  | "AreEqual" // Are its inputs equal
-  | "MakeArray" // Return an array made of its inputs. Input arrays are made an entry in the result
-  | "ConcatArrays" // Return the array made from concatenating its input arrays
-  | "ArrayElement" // Select the given element from its input array - can perform the function of if to make mixture distributionProps
-  | "ArrayLength" // Return the length of the input array
-  | "IntRange"; // Generate a vector of integers from first, size, step
+export const FunctionTypeR = Union(
+  Literal("Add"), // Add its numeric inputs
+  Literal("Multiply"), // Multiply its numeric inputs
+  Literal("Divide"), // Divide its numeric inputs by the last one
+  Literal("Round"), // Round its numeric inputs to the nearest integer
+  Literal("Ceil"), // Round up
+  Literal("Floor"), // Round down
+  Literal("AreEqual"), // Are its inputs equal
+  Literal("MakeArray"), // Return an array made of its inputs. Input arrays are made an entry in the result
+  Literal("ConcatArrays"), // Return the array made from concatenating its input arrays
+  Literal("ArrayElement"), // Select the given element from its input array - can perform the function of if to make mixture distributionProps
+  Literal("ArrayLength"), // Return the length of the input array
+  Literal("IntRange")
+); // Generate a vector of integers from first, size, step
+export type FunctionType = Static<typeof FunctionTypeR>;
 
-export interface NodeBase {
+export const VisualizationTypeR = Union(
+  Literal("1DHistogram"),
+  Literal("2DColorForProbability")
+);
+export type VisualizationType = Static<typeof VisualizationTypeR>;
+
+export const DistributionNodeR = Record({
   // The unique title string (which is also the node id) is the key in the Nodes map
-  type: NodeType;
-  justification: string;
-  coords: number[];
-}
-
-export interface NodeWithParents extends NodeBase {
+  justification: String,
+  coords: Array(Number),
   /// The parents of the given node ... along with which parameter
   /// they attach to.
   ///
   /// I made parents part of the node structure rather than having
   /// separate edge objects because I think it will be easier
   /// to follow in the Github diffs
-  parents: {
-    [inputName: string]: string[]; //Parent ids
-  };
-}
+  parents: Dictionary(Array(String), "string"),
+  type: Literal("DistributionNode"),
+  distribution: DistributionTypeR,
+});
+export type DistributionNode = Static<typeof DistributionNodeR>;
 
-export interface DistributionNode extends NodeWithParents {
-  type: "DistributionNode";
-  distribution: DistributionType;
-}
-
-export interface FunctionNode extends NodeWithParents {
-  type: "FunctionNode";
-  function: FunctionType;
-}
+export const FunctionNodeR = Record({
+  // The unique title string (which is also the node id) is the key in the Nodes map
+  justification: String,
+  coords: Array(Number),
+  /// The parents of the given node ... along with which parameter
+  /// they attach to.
+  ///
+  /// I made parents part of the node structure rather than having
+  /// separate edge objects because I think it will be easier
+  /// to follow in the Github diffs
+  parents: Dictionary(Array(String), "string"),
+  type: Literal("FunctionNode"),
+  function: FunctionTypeR,
+});
+export type FunctionNode = Static<typeof FunctionNodeR>;
 
 /// The names of the primitive types for the Belief Workbench graph language
-export type PrimitiveType = "bool" | "int" | "double" | "string" | "array";
+export const PrimitiveTypeR = Union(
+  Literal("bool"),
+  Literal("int"),
+  Literal("double"),
+  Literal("string"),
+  Literal("array")
+);
+export type PrimitiveType = Static<typeof PrimitiveTypeR>;
 
 /// The Javascript type of a variable that can hold all PrimitiveType
-export type PrimitiveActualType =
-  | boolean
-  | number
-  | string
-  | PrimitiveActualType[];
+export const PrimitiveActualTypeR0 = Union(Boolean, Number, String);
+export const PrimitiveActualTypeR1 = Union(
+  PrimitiveActualTypeR0,
+  Array(PrimitiveActualTypeR0)
+);
+export const PrimitiveActualTypeR2 = Union(
+  PrimitiveActualTypeR1,
+  Array(PrimitiveActualTypeR1)
+);
+export const PrimitiveActualTypeR3 = Union(
+  PrimitiveActualTypeR2,
+  Array(PrimitiveActualTypeR2)
+);
+export const PrimitiveActualTypeR4 = Union(
+  PrimitiveActualTypeR3,
+  Array(PrimitiveActualTypeR3)
+);
+export const PrimitiveActualTypeR5 = Union(
+  PrimitiveActualTypeR4,
+  Array(PrimitiveActualTypeR4)
+);
+export const PrimitiveActualTypeR6 = Union(
+  PrimitiveActualTypeR5,
+  Array(PrimitiveActualTypeR5)
+);
+export const PrimitiveActualTypeR = Union(
+  PrimitiveActualTypeR6,
+  Array(PrimitiveActualTypeR6)
+);
+export type PrimitiveActualType = Static<typeof PrimitiveActualTypeR>;
 
-export interface ConstantNode extends NodeBase {
-  type: "ConstantNode";
-  value: PrimitiveActualType;
-}
+export const ConstantNodeR = Record({
+  // The unique title string (which is also the node id) is the key in the Nodes map
+  justification: String,
+  coords: Array(Number),
+  type: Literal("ConstantNode"),
+  value: PrimitiveActualTypeR,
+});
+export type ConstantNode = Static<typeof ConstantNodeR>;
 
-export type VisualizationType = "1DHistogram" | "2DColorForProbability";
+export const VisualizationNodeR = Record({
+  // The unique title string (which is also the node id) is the key in the Nodes map
+  justification: String,
+  coords: Array(Number),
+  /// The parents of the given node ... along with which parameter
+  /// they attach to.
+  ///
+  /// I made parents part of the node structure rather than having
+  /// separate edge objects because I think it will be easier
+  /// to follow in the Github diffs
+  parents: Dictionary(Array(String), "string"),
+  type: Literal("VisualizationNode"),
+  visualization: VisualizationTypeR,
+});
+export type VisualizationNode = Static<typeof VisualizationNodeR>;
 
-export interface VisualizationNode extends NodeWithParents {
-  type: "VisualizationNode";
-  visualization: VisualizationType;
-}
+export const NodeWithParentsR = Union(
+  DistributionNodeR,
+  FunctionNodeR,
+  VisualizationNodeR
+);
+export type NodeWithParents = Static<typeof NodeWithParentsR>;
 
-export type Node =
-  | DistributionNode
-  | FunctionNode
-  | ConstantNode
-  | VisualizationNode;
-
-/**
- * Return true if this type of node has parents. Type reflection would
- * be better, but it is so clunky in Javascript that I'm just going
- * to deal with the maintenance headaches of not having it
- * @param type The type being queried
- */
-export function hasParents(type: NodeType) {
-  return type !== "ConstantNode";
-}
+export const NodeR = Union(NodeWithParentsR, ConstantNodeR);
+export type Node = Static<typeof NodeR>;
 
 export interface Nodes {
   [node_id: string]: Node;
